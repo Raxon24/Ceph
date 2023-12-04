88d5830b9c065c37b1540a5de6bc035cbe4d7ef5
Rax <61168967+Raxon24@users.noreply.github.com>
2023-12-04 13:47:15 -0500

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+# this is for debian bullseye 
+
+# Run as root only
+check_root() {
+  if [[ "$(id -u)" -ne 0 || $(ps -o comm= -p $PPID) == "sudo" ]]; then
+    clear
+    msg_error "Please run this script as root. "
+    echo -e "\nExiting..."
+    sleep 2
+    exit
+  fi
+}
+
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg lsb-release sudo lvm2
+sleep 10
+
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+curl --silent --remote-name --location https://github.com/ceph/ceph/raw/reef/src/cephadm/cephadm
+chmod +x cephadm
+
+sudo ./cephadm add-repo --release quincy
+sleep 5
+sudo ./cephadm install
+
+sudo cephadm bootstrap --mon-ip 10.0.2.55
